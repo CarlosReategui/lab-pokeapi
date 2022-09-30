bdd4e397c0a61d3209d0bc35ae86373b3135920f
CarlosReategui <carlos.reategui@utec.edu.pe>
2022-09-29 22:16:54 -0500

    agregar envio de correos

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const axios = require("axios");
 const pokemon = require("pokemon");
+const sgMail = require("@sendgrid/mail");
 
 const app = express();
 const PORT = 3000;
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+const sendMail = (pokemon) => {
+  const msg = {
+    to: "cbalbuena@utec.edu.pe",
+    from: "maor.roizman2000@gmail.com", // Change to your verified sender
+    subject: "Error 500",
+    text: `Hubo un error al buscar el pokemon ${pokemon}`,
+  };
+
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode);
+      console.log(response[0].headers);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
 const capitalize = (word) => {
   const to_lower = word.toLowerCase();
@@ -16,8 +38,8 @@ const capitalize = (word) => {
 };
 
 app.get("/query", (req, res) => {
+  const pokemon_query_name = req.query.name;
   try {
-    const pokemon_query_name = req.query.name;
     const pokemon_id = pokemon.getId(capitalize(pokemon_query_name));
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokemon_id}`)
@@ -38,10 +60,12 @@ app.get("/query", (req, res) => {
         });
       })
       .catch((error) => {
+        sendMail(pokemon_query_name);
         res.statusCode = 500;
         res.send("Error al buscar el pokemon.");
       });
   } catch (error) {
+    sendMail(pokemon_query_name);
     res.statusCode = 500;
     res.send("Error al buscar el pokemon.");
   }
